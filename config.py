9dc90e4e138b7fc81da2896fbaa371cab4bef73e
danipixel121 <156018182+Danipixel121@users.noreply.github.com>
2026-03-03 07:54:41 +0100

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-TOKEN = "8729305812:AAH20AxLHKieabFFFFlU77bYlEmEmQdwZ5Y"
-CHAT_ID = "8057839148"
-BUSQUEDA = "mando ps4 original"
-PRECIO_MAX = 15
-ARCHIVO_IDS = "enviados.json"
-SLEEP_TIME = 600  # segundos entre búsquedas
